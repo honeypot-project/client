a601dc68047374c1021f48c17db60449e3942988
dotlineX <59593757+dotlineX@users.noreply.github.com>
2022-11-02 17:01:32 +0100

    added register page

---
+++ assets/js/api-communication-abstractor.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function fetchFromServer(path, httpVerb, requestBody) {
+    const options = constructOptions(httpVerb, requestBody);
+
+    return fetch(`${_config.getAPIUrl()}${path}`, options)
+        .then((response) => {
+            if (!response.ok) {
+                console.error("api scuffed");
+                throw response;
+            }
+            return response.json();
+        })
+        .then((responseToParse) => {
+            return responseToParse;
+        });
+}
+
+function constructOptions(httpVerb, requestBody) {
+    const options = {
+        method: httpVerb,
+        headers: {"Content-Type": "application/json"},
+    };
+
+    // Don't forget to add data to the body when needed
+    options.body = JSON.stringify(requestBody);
+    return options;
+}
+
